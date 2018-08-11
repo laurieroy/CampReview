4c362cf2d481660f54ffc64f54c5d00d8f48f191
laurieroy <laurieroy.dev@gmail.com>
2018-08-11 11:19:13 +0000

    Added in dynamic pricing feature

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -16,13 +16,14 @@ router.get("/", function (req, res) {
 //  CREATE
 router.post("/", middleware.isLoggedIn, function(req, res){
     var name = req.body.name;
+    var price = req.body.price;
     var image = req.body.image;
     var description = req.body.description;
     var author = {
         id: req.user._id,
         username: req.user.username
     }   ;
-    var newCampground = {name: name, image: image, description: description, author: author}; // matching previous format above
+    var newCampground = {name: name, price:price, image: image, description: description, author: author}; // matching previous format above
 
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
